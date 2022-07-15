f0e319bd8b821864a3b0431cb1b2c2b22d4bd5f3
Daiane Silva <104440611+msilvadaiane@users.noreply.github.com>
2022-07-15 14:40:09 -0300

    style-Cadatrar

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Categoria } from '../model/Categoria';
+import { Produto } from '../model/Produto';
+import { CategoriaService } from '../service/categoria.service';
+import { ProdutoService } from '../service/produto.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,16 +13,59 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  listaProdutos: Produto[];
+  listaCategorias: Categoria[];
+  
+  categoria: Categoria;
+  listaFrutas: Produto[];
+  descricao: string;
+
+
   constructor(
-    private router: Router
+    private router: Router,
+    private produtoService: ProdutoService,
+    private categoriaService: CategoriaService
   ) { }
 
   ngOnInit() {
-    window.scroll(0,0)
+    window.scroll(0, 0)
 
-    if(environment.token == '')
+    if (environment.token == '')
       this.router.navigate(['/entrar'])
-    
 
+    this.findAllProdutos();
+    this.findByDescricao();
+    this.findAllCategoria();
+
+
+  }
+
+  // findAllFrutas() {
+  //   this.produtoService.getAllProdutos().subscribe((resp: Produto[]) => {
+  //     this.listaProdutos = resp;
+  //   })
+  //   this.listaFrutas = this.listaProdutos.filter(obj => obj.categoria.tipo == 'frutas');
+
+  // }
+
+  findByDescricao(){
+    this.produtoService.getByDescricao('frutas frescos').subscribe((res: any[]) => {
+      this.listaFrutas = res;
+    })
+  }
+
+  findAllCategoria(){
+    this.categoriaService.getAllCategorias().subscribe((resp: Categoria[]) => {
+      this.listaCategorias = resp;
+    })
+  }
+
+
+
+
+  findAllProdutos() {
+    this.produtoService.getAllProdutos().subscribe((resp: Produto[]) => {
+      this.listaProdutos = resp;
+    })
   }
 }
